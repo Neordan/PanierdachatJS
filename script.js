1383bb49f0073a6007ff62108b2fd12806c389be
Neordan <melhian.adam@gmail.com>
2023-11-07 09:06:09 +0100

    Ajout des paramètres et des instructions pour la fonction calculTotalLine()

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     function calculTotalLine(productLine) {
+        let price = parseFloat(productLine.querySelector('.price').textContent);
+        let quantity = parseInt(productLine.querySelector('.quantity').value);
 
+        let total = (price * quantity);
+        productLine.querySelector('.totalLine').textContent = total + '€';
+        calculTotalArticles();
     }
 })
