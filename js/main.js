ab622f00f4eeebacd1fc84106ffdefa592f4ab0c
Neordan <melhian.adam@gmail.com>
2023-12-06 15:10:32 +0100

    Instanciation des objets à l'aide des classes créées

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const lineAction = new LineAction();
+    const cart = new Cart();
 
     // Modifier la quantité des produits en direct
     function calculTotalLine(tr_cart_product) {
         let quantity = parseInt(tr_cart_product.querySelector('.quantity input').value);
-        
+
         if (quantity <= 0) {
             tr_cart_product.remove();
         } else {
             let unit_price = parseFloat(tr_cart_product.querySelector('.unit_price').getAttribute('data-unit-price'));
-            let total = quantity * unit_price;
+
+            // Créer une instance de la classe Line
+            let line = new Line(unit_price, quantity);
+
+            // Utiliser la méthode calculateTotalLine
+            let total = line.calculateTotalLine();
+
             tr_cart_product.querySelector('.total_price').textContent = total + '€';
             calculTotalLines();
         }
@@ -16,37 +24,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Supprimer un produit du panier
     function deleteProduct(tr_cart_product) {
-        tr_cart_product.querySelector('.remove').addEventListener('click', function () {
-            tr_cart_product.remove();
-            calculTotalLines();
-        });
+        // Utilisation de la méthode deleteProduct de la classe LineAction
+        lineAction.deleteProduct(cart, tr_cart_product);
     }
+    // Créer une instance de la classe Shipment
+    const shipment = new Shipment();
 
     /**
      * Calcule des frais de livraison
      */
     function calculTotalDelivery() {
-        let deliveryChoice = document.getElementById('delivery_choice').value || relais; 
-        let deliveryPrice = deliveryChoice === 'relais' ? 5 : 12;
+        let deliveryChoice = document.getElementById('delivery_choice').value || 'relais';
+
+        //  définir le choix de livraison
+        shipment.setDeliveryChoice(deliveryChoice);
+
+        // Frais de livraison de l'instance Shipment
+        let deliveryPrice = shipment.price;
+
         document.getElementById('deliveryPrice').textContent = deliveryPrice + '€';
         calculTotalLines();
     }
-    
+
+    // Appeler la fonction pour la première fois
     calculTotalDelivery();
-    
+
+
     // Gérer les changements de livraison
     document.getElementById('delivery_choice').addEventListener('change', calculTotalDelivery);
 
 
     // Calculer dynamiquement le total du panier
     function calculTotalLines() {
-        let total = 0;
-        document.querySelectorAll('.cart_product .total_price').forEach(function (item) {
-            total += parseFloat(item.textContent);
-        });
-        let deliveryPrice = parseFloat(document.getElementById('deliveryPrice').textContent);
-        document.getElementById('totalWithDelivery').textContent = (total + deliveryPrice) + '€';
-        document.querySelector('.total_cart').textContent = total + '€';
+        cart.calculateTotalLines(); 
     }
 
     async function displayProducts() {
@@ -54,11 +64,11 @@ document.addEventListener("DOMContentLoaded", function () {
             const response = await fetch('products.json');
             const products = await response.json();
             const tbody = document.querySelector('tbody');
-    
+
             products.forEach(product => {
                 createLineProduct(product, tbody);
             });
-    
+
             // Appel pour calculer les totaux dès le chargement de la page
             calculTotalLines();
         } catch (error) {
@@ -69,8 +79,8 @@ document.addEventListener("DOMContentLoaded", function () {
     function createLineProduct(product, DOM_location) {
         let lineProduct = document.createElement('tr');
         lineProduct.classList.add('cart_product');
-        lineProduct.innerHTML += 
-        `
+        lineProduct.innerHTML +=
+            `
             <td>${product.name}</td>
             <td class="unit_price" data-unit-price="${product.price}">
                 <span class="value">${product.price}</span> €
@@ -84,21 +94,21 @@ document.addEventListener("DOMContentLoaded", function () {
                 <button class="remove">X</button>
             </td>
         `
-        ;
+            ;
 
         DOM_location.appendChild(lineProduct);
 
         let quantityInput = lineProduct.querySelector('.quantity input');
         let removeButton = lineProduct.querySelector('.remove');
-    
+
         quantityInput.addEventListener('input', () => {
             calculTotalLine(lineProduct);
         });
-    
+
         removeButton.addEventListener('click', () => {
             deleteProduct(lineProduct);
         });
-    
+
         // Calculer le total initial pour chaque ligne
         calculTotalLine(lineProduct);
     }
